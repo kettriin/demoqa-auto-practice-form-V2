c302dee6f7753c6413164aad9e0944ec723a5bdc
kettriin <e.shch1904@gmail.com>
2025-11-15 09:10:56 +0300

    allure

--- src/test/java/tests/FillFormTestsV2.java
+++ src/test/java/tests/FillFormTestsV2.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pages.LkForm;
 import pages.components.Buttons;
 import pages.components.Modals;
@@ -23,6 +24,15 @@ public class FillFormTestsV2 {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments(
+                        "--no-sandbox",
+                        "--disable-dev-shm-usage",
+                        "--disable-gpu",
+                        "--headless", // если нужен headless режим
+                        "--window-size=1920,1080"
+                );
     }
 
     @Test
