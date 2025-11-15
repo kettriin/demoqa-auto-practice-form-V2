3d326d857e545e9085a830d1153aa083c7a5d5e1
kettriin <e.shch1904@gmail.com>
2025-11-15 09:27:36 +0300

    К тесту добавлены степы

--- src/test/java/tests/FillFormTestsV2.java
+++ src/test/java/tests/FillFormTestsV2.java
@@ -11,6 +11,7 @@ import pages.components.Modals;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 @Tag("REGISTRATION")
 public class FillFormTestsV2 {
@@ -30,44 +31,52 @@ public class FillFormTestsV2 {
                         "--no-sandbox",
                         "--disable-dev-shm-usage",
                         "--disable-gpu",
-                        "--headless", // если нужен headless режим
+                        "--headless",
                         "--window-size=1920,1080"
                 );
     }
 
     @Test
     void positiveFormFilling() {
-        lkForm.goToPage()
-                .removeAddBannersFromFooter()
-                .nameSurnameFilling("Marry", "By")
-                .emailFilling("mbemail@mail.ru")
-                .genderSetting("Female")
-                .phoneNumberFilling("9384109813")
-                .birthDateSetting("01","October","1991")
-                .subjectInput("b")
-                .hobbiesInpit("Sports")
-                .hobbiesInpit("Reading")
-                .hobbiesInpit("Music")
-                .uploadPicture("png.png")
-                .locationDropdowns("NCR", "Noida")
-                .currentAddressInput("Stretty Street 128");
-
-        submitButton.buttonClick();
+        step("Open form", () -> {
+            lkForm.goToPage()
+                    .removeAddBannersFromFooter()
+                    .nameSurnameFilling("Marry", "By");
+            });
+        step("Filling the form", () -> {
+            lkForm.emailFilling("mbemail@mail.ru")
+                            .genderSetting("Female")
+                            .phoneNumberFilling("9384109813")
+                            .birthDateSetting("01", "October", "1991")
+                            .subjectInput("b")
+                            .hobbiesInpit("Sports")
+                            .hobbiesInpit("Reading")
+                            .hobbiesInpit("Music")
+                            .uploadPicture("png.png")
+                            .locationDropdowns("NCR", "Noida")
+                            .currentAddressInput("Stretty Street 128");
+                });
+        step("Submit the form", () -> submitButton.buttonClick());
 
         // проверяем появилась ли модалка с введенными данными
-        dataModal.checkModalVisible("Thanks for submitting the form");
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        step("Check the modal with saved data", () -> {
+            dataModal.checkModalVisible("Thanks for submitting the form");
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+                });
+
         // через проверку содержимого нужных нам ячеек в таблице
-        dataModal.checkSavedData("Student Name", "Marry By")
-                .checkSavedData("Student Email", "mbemail@mail.ru")
-                .checkSavedData("Gender", "Female")
-                .checkSavedData("Mobile", "9384109813")
-                .checkSavedData("Date of Birth", "01 November,1991")
-                .checkSavedData("Subjects", "Biology")
-                .checkSavedData("Hobbies", "Sports, Reading, Music")
-                .checkSavedData("Picture", "png.png")
-                .checkSavedData("Address", "Stretty Street 128")
-                .checkSavedData("State and City", "NCR Noida");
+        step("Check the saved data", () -> {
+            dataModal.checkSavedData("Student Name", "Marry By")
+                    .checkSavedData("Student Email", "mbemail@mail.ru")
+                    .checkSavedData("Gender", "Female")
+                    .checkSavedData("Mobile", "9384109813")
+                    .checkSavedData("Date of Birth", "01 November,1991")
+                    .checkSavedData("Subjects", "Biology")
+                    .checkSavedData("Hobbies", "Sports, Reading, Music")
+                    .checkSavedData("Picture", "png.png")
+                    .checkSavedData("Address", "Stretty Street 128")
+                    .checkSavedData("State and City", "NCR Noida");
+        });
     }
 
     @Test
